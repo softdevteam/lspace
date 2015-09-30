b650b837f92428c186e5aa417bbd379ee9acbb91
Geoffrey French <frondit1985@gmail.com>
2015-09-30 16:45:41 +0100

    Elements now refer to their children through `Rc<RefCell<Box<TElement>>>`. This allows the element tree to be mutated.

--- src/lib/lspace/elements/column.rs
+++ src/lib/lspace/elements/column.rs
@@ -1,5 +1,7 @@
 use cairo::Context;
 
+use std::cell::{Ref, RefMut};
+
 use layout::lreq::LReq;
 use layout::lalloc::LAlloc;
 use layout::vertical_layout;
@@ -48,14 +50,16 @@ impl TElement for ColumnElement {
 
     fn update_x_req(&mut self) {
         self.update_children_x_req();
-        let child_x_reqs: Vec<&LReq> = self.children.iter().map(|c| c.get().x_req()).collect();
+        let child_refs: Vec<Ref<Box<TElement>>> = self.children.iter().map(|c| c.get()).collect();
+        let child_x_reqs: Vec<&LReq> = child_refs.iter().map(|c| c.x_req()).collect();
         self.req.x_req = vertical_layout::requisition_x(&child_x_reqs);
     }
 
     fn allocate_x(&mut self) {
         {
-            let mut x_pairs: Vec<(&LReq, &mut LAlloc)> = self.children.iter_mut().map(
-                    |c| c.get_mut().x_req_and_mut_alloc()).collect();
+            let mut child_refs: Vec<RefMut<Box<TElement>>> = self.children.iter_mut().map(|c| c.get_mut()).collect();
+            let mut x_pairs: Vec<(&LReq, &mut LAlloc)> = child_refs.iter_mut().map(
+                    |c| c.x_req_and_mut_alloc()).collect();
             vertical_layout::alloc_x(&self.req.x_req,
                     &self.alloc.x_alloc.without_position(), &mut x_pairs);
         }
@@ -64,14 +68,16 @@ impl TElement for ColumnElement {
 
     fn update_y_req(&mut self) {
         self.update_children_y_req();
-        let child_y_reqs: Vec<&LReq> = self.children.iter().map(|c| c.get().y_req()).collect();
+        let child_refs: Vec<Ref<Box<TElement>>> = self.children.iter().map(|c| c.get()).collect();
+        let child_y_reqs: Vec<&LReq> = child_refs.iter().map(|c| c.y_req()).collect();
         self.req.y_req = vertical_layout::requisition_y(&child_y_reqs, self.y_spacing, None);
     }
 
     fn allocate_y(&mut self) {
         {
-            let mut y_pairs: Vec<(&LReq, &mut LAlloc)> = self.children.iter_mut().map(
-                    |c| c.get_mut().y_req_and_mut_alloc()).collect();
+            let mut child_refs: Vec<RefMut<Box<TElement>>> = self.children.iter_mut().map(|c| c.get_mut()).collect();
+            let mut y_pairs: Vec<(&LReq, &mut LAlloc)> = child_refs.iter_mut().map(
+                    |c| c.y_req_and_mut_alloc()).collect();
             vertical_layout::alloc_y(&self.req.y_req,
                     &self.alloc.y_alloc.without_position(),
                     &mut y_pairs, self.y_spacing, None);
